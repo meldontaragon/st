1128f5c7de683e4f918e7704148b9dff8ab296af
Mel Miller <taragon.meldon@gmail.com>
2019-09-30 23:12:02 -0400

    Updated some values in config.mk, might break on non Arch systems

--- config.mk
+++ config.mk
@@ -9,8 +9,11 @@ PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 FONTPREFIX = $(PREFIX)/share/fonts/truetype
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 PKG_CONFIG = pkg-config
 
@@ -24,24 +27,11 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
 
 # flags
 
-# old
-#CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -g3 -Og
-#STCFLAGS = $(INCS) $(CPPFLAGS) $(CFLAGS)
-
 #new
-#STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -g3 -Og
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -g3 -O0
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 
 STLDFLAGS = $(LIBS) $(LDFLAGS)
-
-# OpenBSD:
-#CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-#       `pkg-config --libs fontconfig` \
-#       `pkg-config --libs freetype2`
-
-# compiler and linker
 
 CC = gcc
 # CC = c99
